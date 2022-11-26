c8c535c6df3136ec29cbedc5f8bbc27dad43a981
Francois Burianek <spartanlaser@live.com>
2022-11-26 10:37:56 -0800

    provided three login options - manual, basic, and full

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ namespace OptumHsaSaveItExport
     {
         static void Main(string[] args)
         {
+            if (!HandleArgs(args)) return;
+
             UiModel ui = UiModel.GetInstance();
             DataModel data = DataModel.GetInstance();
             try
@@ -35,6 +37,30 @@ namespace OptumHsaSaveItExport
             {
                 ui.Quit();
             }
+        }
+
+        static bool HandleArgs(string[] args)
+        {
+            bool argsAreCorrect = true;
+            if (args.Length > 0)
+            {
+                LoginType login;
+                argsAreCorrect = Enum.TryParse<LoginType>(args[0].ToLower(), out login);
+                Settings.Login = login;
+            }
+            else
+            {
+                Console.WriteLine("No args passed. Defaulting to {0}", Settings.Login);
+            }
+            if (!argsAreCorrect)
+            {
+                Console.WriteLine("Args are incorrect\nPass the following optional parameters" +
+                    "\n manualLogin - you will be responsible for logging in and getting the browser to the optum site and HSA Save-it piggybank url" +
+                    "\n basicLogin - you will be responsible for authenticating with SSO via benefits ehr and clicking Yes/No to staying logged in" +
+                    "\n fullLogin - you will be responsible for only interacting with your 2FA/MFA device, everything else is handled for you");
+            }
+            return argsAreCorrect;
+
         }
 
     }
